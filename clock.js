0e706b62e53ad155e06765e19b6a3bab465393c9
gusholguin <augustin.holguin@gmail.com>
2017-10-06 12:56:15 +0200

    Date setup

--- clock.js
+++ clock.js
@@ -2,9 +2,14 @@
 
 var today = new Date(); //Date objects are created with new Date()
 var day = today.getDay();
-var dayList = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var date = today.getDate();
+var monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var month = today.getMonth();
+var year = today.getFullYear();
 var hour = today.getHours();
 var minute = today.getMinutes();
 var second = today.getSeconds();
 
+document.write("Today is : "+dayList[day] + ',' +' '+ date + ' ' + 'of' + ' '+ monthList[month] + ' ' + year +"<br>");
 document.write("Current Time : "+hour + 'h' + " : " + minute + " : " + second);
