29d829283665672e713f565e32342be1be6b20ec
gusholguin <augustin.holguin@gmail.com>
2017-10-06 12:38:32 +0200

    current time working

--- clock.js
+++ clock.js
@@ -1,2 +1,10 @@
-var gus = 'hello';
-console.log(gus);
+// A JavaScript program to display the current day and time
+
+var today = new Date(); //Date objects are created with new Date()
+var day = today.getDay();
+var dayList = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var hour = today.getHours();
+var minute = today.getMinutes();
+var second = today.getSeconds();
+
+document.write("Current Time : "+hour + 'h' + " : " + minute + " : " + second);
