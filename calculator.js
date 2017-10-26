125a867d8c76833c03b142a66151865c06d4569b
gusholguin <augustin.holguin@gmail.com>
2017-10-26 17:20:29 +0200

    multiplier and divider working

--- calculator.js
+++ calculator.js
@@ -1,3 +1,11 @@
-function calculator {
-  
+function multiplyBy() {
+  var firstNumber = document.getElementById('number1').value;
+  var secondNumber = document.getElementById('number2').value;
+  document.getElementById("result").innerHTML = firstNumber * secondNumber;
+}
+
+function divideBy() {
+  var firstNumber = document.getElementById('number1').value;
+  var secondNumber = document.getElementById('number2').value;
+  document.getElementById("result").innerHTML = firstNumber / secondNumber;
 }
