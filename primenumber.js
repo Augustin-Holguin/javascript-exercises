fc25fd16d93f501ef566fb835b5161be516fe379
gusholguin <augustin.holguin@gmail.com>
2017-10-29 13:24:52 +0100

    primenumber init

---
+++ primenumber.js
@@ -0,0 +1,8 @@
+// A Javascript program to check if a given number is prime
+
+function primeNumberChecker() {
+  var number = document.getElementById('primeNumber').value;
+  if (number) {
+    
+  }
+}
