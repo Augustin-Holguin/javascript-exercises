245cb7ab416ce2512356b4e526b16e163b8f14af
gusholguin <augustin.holguin@gmail.com>
2017-10-29 15:33:38 +0100

    prime number checker done

--- primenumber.js
+++ primenumber.js
@@ -2,7 +2,19 @@
 
 function primeNumberChecker() {
   var number = document.getElementById('primeNumber').value;
-  if (number) {
-    
+  if (number == 0 || number == 1) {
+    document.getElementById("primeNumberResult").innerHTML = "No !";
+  } else if (number === 2 ||
+             number === 3 ||
+             number > 2 && number % 2 > 0 && number % 3 > 0 ||
+             number > 3 && number % 2 > 0 && number % 3 > 0) {
+    document.getElementById("primeNumberResult").innerHTML = "Yes !";
+  } else {
+    for (i = 2; i < number; i++) {
+      if (number % i == 0) {
+        document.getElementById("primeNumberResult").innerHTML = "No !";
+        break;
+      }
+    }
   }
 }
