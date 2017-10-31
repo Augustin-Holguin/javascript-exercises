a1c6597ace120bdf5c6662e0a9a1ee50547f7de6
gusholguin <augustin.holguin@gmail.com>
2017-10-31 14:51:57 +0100

    number sorter init

--- gcd.js
+++
@@ -1,5 +0,0 @@
-// A Javascript program to find the Greatest Common Divisor of two given numbers
-
-function gcd {
-  
-}
---
+++ numbersorter.js
@@ -0,0 +1,5 @@
+// A Javascript program to sort number from the lowest to the greatest
+
+var x = document.getElementById('numberOne').value;
+var y = document.getElementById('numberTwo').value;
+var z = document.getElementById('numberThree').value;
