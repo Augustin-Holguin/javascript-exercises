7e5396e3d433ff5b7a9cf87612444fcc069bd80c
gusholguin <augustin.holguin@gmail.com>
2017-10-31 15:38:18 +0100

    number sorter done

--- numbersorter.js
+++ numbersorter.js
@@ -1,5 +1,37 @@
 // A Javascript program to sort number from the lowest to the greatest
 
+function numberSorter() {
+
 var x = document.getElementById('numberOne').value;
 var y = document.getElementById('numberTwo').value;
 var z = document.getElementById('numberThree').value;
+var numberArray = [x,y,z];
+
+document.getElementById("numberSorted").innerHTML = numberArray.sort(function(a,b) { return a - b; });
+}
+
+
+// Not working with function
+/*
+if (a > y && a > z) {
+  if (y > z) {
+    document.getElementById("numberSorted").innerHTML = z + " < " + y + " < " + a;
+  } else {
+    document.getElementById("numberSorted").innerHTML = y + " < " + z + " < " + a;
+  }
+}
+else if (y > a && y > z) {
+  if (a > z) {
+    document.getElementById("numberSorted").innerHTML = z + " < " + a + " < " + y;
+  } else {
+    document.getElementById("numberSorted").innerHTML = a + " < " + z + " < " + y;
+  }
+}
+else if (z > a && z > y) {
+  if (a > y) {
+    document.getElementById("numberSorted").innerHTML = y + " < " + a + " < " + z;
+  } else {
+    document.getElementById("numberSorted").innerHTML = a + " < " + y + " < " + z;
+  }
+}
+*/
