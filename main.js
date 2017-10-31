f7d36364fbdf3e4f5f49ef837b0271cbad6f08cb
gusholguin <augustin.holguin@gmail.com>
2017-10-31 18:37:11 +0100

    navbar animation jquery

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 function scrollUp() {
   document.body.scrollTop = document.documentElement.scrollTop = 0;
 }
+
+$(document).ready(function(){
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $("header").css('background', '#f4f7f9');
+          $("header").css('color', '#59626a');
+          $("header img").css('visibility', 'visible');
+       } else {
+          $("header").css('background', '#f65a5b');
+          $("header").css('color', '#f65a5b');
+          $("header img").css('visibility', 'hidden');
+       }
+   });
+    }
+});
