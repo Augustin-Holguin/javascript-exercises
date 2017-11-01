07456f322a38652a46f5b3f3ea4fd3c35a07fd9f
gusholguin <augustin.holguin@gmail.com>
2017-11-01 17:23:52 +0100

    birthday exercice init

---
+++ birthday.js
@@ -0,0 +1,9 @@
+// A JavaScript program to figure what day of the week was you were born
+
+function findBirthday() {
+  var dayB = document.getElementById('day').value;
+  var monthB = document.getElementById('month').value;
+  var yearB = document.getElementById('year').value;
+  var myDays= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+  var birthday =
+}
