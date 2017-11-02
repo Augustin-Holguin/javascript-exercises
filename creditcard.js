fed7b2b63efc0e05687251b319a703bd0b789eaf
gusholguin <augustin.holguin@gmail.com>
2017-11-02 17:30:05 +0100

    credit card validation 2.0

--- creditcard.js
+++ creditcard.js
@@ -2,8 +2,8 @@
 
 function creditCardValidation() {
 
-  var cardNumber = document.getElementById('cardnumber').value;
-  regexp = /(\d{16})$/g;
+var cardNumber = document.getElementById('cardnumber').value;
+regexp = /^\d{16}$/;
 
   if (regexp.test(cardNumber)) {
     document.getElementById("cardValid").innerHTML = "Your card is valid !";
