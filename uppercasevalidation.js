3b72e182322a7e83d2249715403379aa8c14007a
gusholguin <augustin.holguin@gmail.com>
2017-11-02 15:39:01 +0100

    Uppercase validation regexp done

--- uppercasevalidation.js
+++ uppercasevalidation.js
@@ -1,5 +1,11 @@
 // A JavaScript program to test the first character of a string is uppercase or not
 
 function isUppercase() {
-  
+  var stringToCheck = document.getElementById('string').value;
+  regexp = /^[A-Z]/;
+  if (regexp.test(stringToCheck)) {
+    document.getElementById("UppercaseOrNot").innerHTML = "Your string's first character is uppercase !";
+  } else {
+    document.getElementById("UppercaseOrNot").innerHTML = "Your string's first character is not uppercase ...";
+  }
 }
