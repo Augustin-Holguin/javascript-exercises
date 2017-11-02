3d56697f2b7e6299bfff7a8f5ce875abdcaf4803
gusholguin <augustin.holguin@gmail.com>
2017-11-02 15:18:03 +0100

    uppercase validation w/ RegExp

--- birthday.js
+++
@@ -1,9 +0,0 @@
-// A JavaScript program to figure what day of the week was you were born
-
-function findBirthday() {
-  var dayB = document.getElementById('day').value;
-  var monthB = document.getElementById('month').value;
-  var yearB = document.getElementById('year').value;
-  var myDays= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-  var birthday =
-}
---
+++ uppercasevalidation.js
@@ -0,0 +1,5 @@
+// A JavaScript program to test the first character of a string is uppercase or not
+
+function isUppercase() {
+  
+}
