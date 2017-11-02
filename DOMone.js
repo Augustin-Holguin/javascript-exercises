3a3b24746bf0c2f7e13653b8a45a76e8b448147b
gusholguin <augustin.holguin@gmail.com>
2017-11-02 17:52:31 +0100

    DOM ex1 done

---
+++ DOMone.js
@@ -0,0 +1,19 @@
+// JavaScript programs to modify the style of a paragraph
+
+function styleOne() {
+  paragraphStyle.style.color = "#ffc20e";
+  paragraphStyle.style.background = "#146eb4";
+  paragraphStyle.style.border = "solid 2px #ce181e";
+}
+
+function styleTwo() {
+  paragraphStyle.style.color = "#146eb4";
+  paragraphStyle.style.background = "#ce181e";
+  paragraphStyle.style.border = "solid 2px #ffc20e";
+}
+
+function styleThree() {
+  paragraphStyle.style.color = "#ce181e";
+  paragraphStyle.style.background = "#ffc20e";
+  paragraphStyle.style.border = "solid 2px #146eb4";
+}
