17e38aa56ca34fe29eaf4ca2d209950a21ff22b2
gusholguin <augustin.holguin@gmail.com>
2017-11-16 18:41:46 +0100

    DOM ex2 done

---
+++ elementremover.js
@@ -0,0 +1,18 @@
+function removeElementOne() {
+  var x = document.getElementById("elre1");
+  x.remove(x.selectedIndex);
+}
+function removeElementTwo() {
+  var y = document.getElementById("elre2");
+  y.remove(y.selectedIndex);
+}
+
+function removeElementThree() {
+  var z = document.getElementById("elre3");
+  z.remove(z.selectedIndex);
+}
+
+function removeElementFour() {
+  var a = document.getElementById("elre4");
+  a.remove(a.selectedIndex);
+}
